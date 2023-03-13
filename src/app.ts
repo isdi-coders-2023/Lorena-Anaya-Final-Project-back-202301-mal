a6bb8f61c4426bd36a013c8bcc871e1925bc0bf3
LorenaAnayaDiaz <lorena.anaya.diaz@gmail.com>
2023-03-13 19:09:19 +0100

    add app and app test

--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,14 @@
-export const hello = {
-  hello: 'Hello world',
-};
+import express from 'express';
+
+import cors from 'cors';
+
+const app = express();
+
+app.get('/', (_req, res) => {
+  res.json('Hello world');
+});
+
+app.use(cors());
+app.use(express.json());
+
+export default app;
