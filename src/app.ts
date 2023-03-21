6efcca73c0857c0e2c3392300d95dc9b7da8f195
LorenaAnayaDiaz <lorena.anaya.diaz@gmail.com>
2023-03-21 13:44:14 +0100

    add body parser

--- src/app.ts
+++ src/app.ts
@@ -5,6 +5,7 @@ import { authRouter } from './api/auth/auth-router.js';
 import { appErrorHandler } from './utils/error-handler.js';
 import { UsersRouter } from './api/users/users-router.js';
 import { TranslationsRouter } from './api/translations/translations-router.js';
+import bodyParser from 'body-parser';
 
 const app = express();
 
@@ -14,6 +15,7 @@ app.get('/', (_req, res) => {
 
 app.use(cors());
 app.use(express.json());
+app.use(bodyParser.json());
 
 app.disable('x-powered-by');
 
