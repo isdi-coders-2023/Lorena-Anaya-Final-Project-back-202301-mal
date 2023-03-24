65fc5f8330cfd6e1847718fa95ece0bcd4e2ffeb
LorenaAnayaDiaz <lorena.anaya.diaz@gmail.com>
2023-03-24 19:11:56 +0100

    add get user by id route

--- src/api/users/users-router.ts
+++ src/api/users/users-router.ts
@@ -1,5 +1,6 @@
 import express from 'express';
 import {
+  getUserByIdController,
   getUsersController,
   getUserTranslationsController,
 } from './users-controller.js';
@@ -8,3 +9,4 @@ export const UsersRouter = express.Router();
 
 UsersRouter.route('/:id/translations').get(getUserTranslationsController);
 UsersRouter.route('/all').get(getUsersController);
+UsersRouter.route('/:id/name').get(getUserByIdController);
